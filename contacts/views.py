ce4388445dc2a3613fec1e0ef550bd4413f61a95
bened <bezunyl@gmail.com>
2024-02-26 09:37:02 +0100

    feat: started working on displaying contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
 
 @login_required(login_url='authentication:login')
-def index(req):
-    return render(req, 'contacts/index.html')
+def index(req: HttpRequest):
+    user_contacts = req.user.contacts.all().filter(is_owner = False)
+
+    return render(req, 'contacts/index.html', { 'user_contacts': user_contacts })
